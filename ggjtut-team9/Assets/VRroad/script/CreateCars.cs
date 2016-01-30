7fdda5a03990206289e600b50e972e37f2dae641
Riguruchan <aaaaaaaaaadddddddddd30@gmail.com>
2016-01-30 20:46:57 +0900

    アルファ前最終統合版

--- ggjtut-team9/Assets/VRroad/script/CreateCars.cs
+++ ggjtut-team9/Assets/VRroad/script/CreateCars.cs
@@ -12,20 +12,24 @@ public class CreateCars : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        count += Time.deltaTime;
-        if (count > 3)
+        int score = GameObject.FindWithTag("Player").GetComponent<CameraMove>().score;
+        if (score > 0)
         {
-            if (Random.Range(0, 2) > 0)
+            count += Time.deltaTime;
+            if (count > Mathf.Max(10.0f / (float)score,4.0f))
             {
-                GameObject obj = Instantiate(car, new Vector3(-34, -1.6f, Random.Range(-1, -7)), new Quaternion(0, 0.7f, 0, 0.7f)) as GameObject;
-                obj.GetComponent<Car>().SetLeft(true);
+                if (Random.Range(0, 2) > 0)
+                {
+                    GameObject obj = Instantiate(car, new Vector3(-34, -1.6f, Random.Range(-1, -7)), new Quaternion(0, 0.7f, 0, 0.7f)) as GameObject;
+                    obj.GetComponent<Car>().SetLeft(true);
+                }
+                else
+                {
+                    GameObject obj = Instantiate(car, new Vector3(34, -1.6f, Random.Range(-18, -13)), new Quaternion(0, -0.7f, 0, 0.7f)) as GameObject;
+                    obj.GetComponent<Car>().SetLeft(false);
+                }
+                count = 0;
             }
-            else
-            {
-                GameObject obj = Instantiate(car, new Vector3(34, -1.6f, Random.Range(-18, -13)), new Quaternion(0, -0.7f, 0, 0.7f)) as GameObject;
-                obj.GetComponent<Car>().SetLeft(false);
-            }
-            count = 0;
         }
 	}
 }
