36ab7421a31fd59d66241d6276c07139dbac80cc
RyunosukeHayashi <m011434688@edu.teu.ac.jp>
2016-01-30 13:30:33 +0900

    シーンとＵＩを追加しました

---
+++ ggjtut-team9/Assets/VRroad/script/StoryControler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StoryControler : MonoBehaviour {
+
+    public Image image;
+    public Sprite[] story = new Sprite[10];
+    int count = 0;
+
+
+
+	// Use this for initialization
+	void Start () {
+        image.sprite = story[0];
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) count++;
+
+        switch (count)
+        {
+            case 1:
+                image.sprite = story[1];
+                break;
+            case 2:
+                image.sprite = story[2];
+                break;
+            case 3:
+                image.sprite = story[3];
+                break;
+            case 4:
+                image.sprite = story[4];
+                break;
+            case 5:
+                image.sprite = story[5];
+                break;
+            case 6:
+                image.sprite = story[6];
+                break;
+
+        }
+
+
+
+    }
+}
