efb169941b6d66a813fbe54bd5e1b7086c7e0866
RyunosukeHayashi <m011434688@edu.teu.ac.jp>
2016-01-31 09:04:55 +0900

    ＵＩを追加

---
+++ ggjtut-team9/Assets/VRroad/script/ScoreCount.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreCount : MonoBehaviour {
+
+    public SpriteRenderer count1;
+    public SpriteRenderer count2;
+
+
+    public Sprite[] count = new Sprite[10];
+
+    GameObject player;
+    CameraMove cameramove;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("player");
+        cameramove = player.GetComponent<CameraMove>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (cameramove.score)
+        {
+            case 0:
+                count1.sprite = count[0];
+                count2.sprite = count[0];
+                break;
+            case 1:
+                count1.sprite = count[1];
+                count2.sprite = count[1];
+                break;
+            case 2:
+                count1.sprite = count[2];
+                count2.sprite = count[2];
+                break;
+            case 3:
+                count1.sprite = count[3];
+                count2.sprite = count[3];
+                break;
+            case 4:
+                count1.sprite = count[4];
+                count2.sprite = count[4];
+                break;
+            case 5:
+                count1.sprite = count[5];
+                count2.sprite = count[5];
+                break;
+            case 6:
+                count1.sprite = count[6];
+                count2.sprite = count[6];
+                break;
+            case 7:
+                count1.sprite = count[7];
+                count2.sprite = count[7];
+                break;
+            case 8:
+                count1.sprite = count[8];
+                count2.sprite = count[8];
+                break;
+            case 9:
+                count1.sprite = count[9];
+                count2.sprite = count[9];
+                break;
+
+        }
+
+    }
+}
