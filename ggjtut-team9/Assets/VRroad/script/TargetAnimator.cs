8a9d5807f33f95743541e3924f6c556b5520b461
遠藤崇 <mikankari@1223066-no-MacBook-Air.local>
2016-01-31 12:42:23 +0900

    targetが発光するぞ

---
+++ ggjtut-team9/Assets/VRroad/script/TargetAnimator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetAnimator : MonoBehaviour {
+
+	private Renderer[] renderers;
+	private Color emissionColor;
+	private float increment = 0.005f;
+	private float min = 0.2f;
+	private float max = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		renderers = gameObject.GetComponentsInChildren<Renderer> ();
+		for (int i = 0; i < renderers.Length; i++) {
+			renderers [i].material.EnableKeyword ("_EMISSION");
+		}
+		float center = ((max - min) / 2.0f) + min;
+		Debug.Log (center);
+		emissionColor = new Color (center, center, center);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (emissionColor.r < min || emissionColor.r > max) {
+			increment *= -1.0f;
+		}
+		emissionColor.r += increment;
+		emissionColor.g += increment;
+		emissionColor.b += increment;
+		for (int i = 0; i < renderers.Length; i++) {
+			renderers [i].material.SetColor ("_EmissionColor", emissionColor);
+		}
+	}
+}
+
