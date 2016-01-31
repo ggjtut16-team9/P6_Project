f34a23590b28e49c9c01895148471758fc2bdd0e
遠藤崇 <mikankari@1223066-no-MacBook-Air.local>
2016-01-31 14:15:15 +0900

    反対側にparticleがいない問題を修正

--- ggjtut-team9/Assets/VRroad/script/Target.cs
+++ ggjtut-team9/Assets/VRroad/script/Target.cs
@@ -3,14 +3,22 @@ using System.Collections;
 
 public class Target : MonoBehaviour {
     private CameraMove cameraMove;
+	private float timer;
+	private bool once;
+	private bool once_old;
+
 	// Use this for initialization
 	void Start () {
         cameraMove = GameObject.FindWithTag("Player").GetComponent<CameraMove>();
+		timer = 0;
+		once = false;
+		once_old = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (cameraMove.PlayerGoEnd())
+		once = cameraMove.PlayerGoEnd ();
+        if (once)
         {
             transform.position = new Vector3(0, 0.3f, 5.0f);
             transform.rotation = new Quaternion(0,0,0,0);
@@ -20,6 +28,15 @@ public class Target : MonoBehaviour {
             transform.position = new Vector3(0, 0.3f, -25.0f);
             transform.rotation = new Quaternion(0,1,0,0);
         }
-
+		if (once != once_old) {
+			// process once
+			once_old = once;
+			timer = 0;
+		}
+		timer += Time.deltaTime;
+		if (timer > 3.0f) {
+			ParticleSystem particle = FindObjectOfType<ParticleSystem> ();
+			particle.transform.position = transform.position;
+		}
 	}
 }
